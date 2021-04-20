aa2ff5af7ae77207697261d1eae00f09ca97cb8c
Turi <40395348+turinaf@users.noreply.github.com>
2021-04-20 22:36:53 +0800

    update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/Header'
+import React from 'react'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         <h2>Hello React</h2>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
     </div>
   );
 }
+// class App extends React.Component {
+//   render(){
+//     return <h1>Hello from App class</h1>
+//   }
+// }
 
 export default App;
